ba3fd6640eea3bb4baf5e66b3206f1b95ad490d8
Derevar <github@3d20.de>
2019-01-24 09:45:59 +0100

    Fixed chat input textbox resizing

---
+++ Ui/Ui.Desktop/CustomControls/ChatInputTextBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PhexensWuerfelraum.Ui.Desktop.CustomControls
+{
+    // https://stackoverflow.com/a/42370393/7557790
+    public class ChatInputTextBox : TextBox
+    {
+        static ChatInputTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ChatInputTextBox), new FrameworkPropertyMetadata(typeof(ChatInputTextBox)));
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            return new Size(0, 0);
+        }
+    }
+}
