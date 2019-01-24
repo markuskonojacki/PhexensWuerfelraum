65d1b936bc97d37bb34d0d2aed0f5824708584be
Derevar <github@3d20.de>
2019-01-24 21:41:06 +0100

    Refactored styles into separate files

---
+++ Ui/Ui.Desktop/CustomControls/ChatMessagesListView.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PhexensWuerfelraum.Ui.Desktop.CustomControls
+{
+    public class ChatMessagesListView : ListView
+    {
+        static ChatMessagesListView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ChatMessagesListView), new FrameworkPropertyMetadata(typeof(ChatMessagesListView)));
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            return new Size(0, 0);
+        }
+    }
+}
