d5cefa80ac7e32595c5168214948bb9fcb92f359
Derevar <github@3d20.de>
2019-01-25 13:07:42 +0100

    Updated copyright

--- .shared/SharedAssembly.cs
+++ .shared/SharedAssembly.cs
@@ -3,7 +3,7 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PhexensWuerfelraum")]
 [assembly: AssemblyProduct("Phexens Würfelraum")]
-[assembly: AssemblyCopyright("Copyright © Markus Konojacki 2018")]
+[assembly: AssemblyCopyright("Copyright © Markus Konojacki 2019")]
 [assembly: AssemblyTrademark("Markus Konojacki")]
 [assembly: AssemblyVersion("2.0.0")]
 [assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
