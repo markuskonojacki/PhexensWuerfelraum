45fa5c4cef14fdec2feb015cfa27901e9c6aad64
Derevar <github@3d20.de>
2019-01-29 16:05:47 +0100

    Increased version

--- .shared/SharedAssembly.cs
+++ .shared/SharedAssembly.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyProduct("Phexens Würfelraum")]
 [assembly: AssemblyCopyright("Copyright © Markus Konojacki 2019")]
 [assembly: AssemblyTrademark("Markus Konojacki")]
-[assembly: AssemblyVersion("2.0.2")]
+[assembly: AssemblyVersion("2.0.3")]
 [assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
